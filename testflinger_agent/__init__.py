aa0678c6ee31b38231fdb6f2847643044cab4b1f
hum4n0id <adrian.lane@canonical.com>
2023-02-14 12:51:35 -0800

    add agent api logging and state capabilities

--- testflinger_agent/__init__.py
+++ testflinger_agent/__init__.py
@@ -17,6 +17,11 @@ import logging
 import os
 import time
 import yaml
+import json
+import requests
+from requests.adapters import HTTPAdapter, Retry
+from urllib.parse import urljoin
+from threading import Thread, Timer
 
 from testflinger_agent import schema
 from testflinger_agent.agent import TestflingerAgent
@@ -26,9 +31,126 @@ from logging.handlers import TimedRotatingFileHandler
 logger = logging.getLogger(__name__)
 
 
+class LoopTimer(Timer):
+    """Loop thread timer (interval)"""
+
+    def run(self):
+        while not self.finished.wait(self.interval):
+            self.function(*self.args, **self.kwargs)
+
+
+class ReqLogger(Thread):
+    """Threaded requests logger"""
+
+    def __init__(self, agent, server):
+        super().__init__()
+        self.daemon = True
+        self.agent = agent
+        self.server = server
+        self.request_handler = None
+        self.config_reqlogger()
+
+    def config_reqlogger(self):
+        """configure logging"""
+        # inherit from logger __name__
+        req_logger = logging.getLogger()
+        # inherit level
+        # req_logger.setLevel(logging.info)
+        request_formatter = ReqFormatter()
+        self.request_handler = ReqBufferHandler(self.agent, self.server)
+        self.request_handler.setFormatter(request_formatter)
+        req_logger.addHandler(self.request_handler)
+
+    def join(self):
+        """cleanup children"""
+        self.request_handler.reqbuf_timer.stop()
+        self.request_handler.close()
+
+
+class ReqBufferHandler(logging.Handler):
+    """Requests logging handler"""
+
+    def __init__(self, agent, server):
+        super().__init__()
+        self.server = server
+        uri = urljoin(server, "/v1/agents/data/")
+        self.url = urljoin(uri, agent)
+        self.buffer = []
+        self.qdepth = 100  # messages
+        self.reqbuf_timer = None
+        self.reqbuf_interval = 10.0  # seconds
+        self._start_rb_timer()
+        # reuse socket
+        self.session = self._requests_retry()
+
+    def _requests_retry(self, retries=3):
+        """retry api server"""
+        session = requests.Session()
+        retry = Retry(
+            total=retries,
+            read=retries,
+            connect=retries,
+            backoff_factor=0.3,
+            status_forcelist=(500, 502, 503, 504),
+        )
+        adapter = HTTPAdapter(max_retries=retry)
+        session.mount("http://", adapter)
+        session.mount("https://", adapter)
+        return session
+
+    def _start_rb_timer(self):
+        """periodically check and send buffer"""
+        self.reqbuf_timer = LoopTimer(self.reqbuf_interval,
+                                      self.flush)
+        self.reqbuf_timer.daemon = True
+        self.reqbuf_timer.start()
+
+    def emit(self, record):
+        """write logging events to buffer (callback)"""
+        if len(self.buffer) >= self.qdepth:
+            self.flush(bfull=True)
+
+        self.buffer.append(record)
+
+    def flush(self, bfull=False):
+        """flush and post buffer"""
+        try:
+            for record in self.buffer:
+                self.session.post(url=self.url,
+                                  data=self.format(record),
+                                  headers={
+                                      "Content-type": "application/json"},
+                                  timeout=3)
+        except Exception as error:
+            logger.exception(error)
+
+            if not bfull:
+                pass
+
+        self.buffer = []
+
+
+class ReqFormatter(logging.Formatter):
+    """Format logging messages"""
+
+    def format(self, record):
+        data = {"log": [record.getMessage()]}
+
+        return json.dumps(data)
+
+
 def start_agent():
     args = parse_args()
     config = load_config(args.config)
+    # requests logging
+    server = config.get("server_address")
+    if not server.lower().startswith("http"):
+        server = "http://" + server
+    reqlog_thread = Thread(target=ReqLogger,
+                           args=(config.get("agent_id"),
+                                 server))
+    reqlog_thread.start()
+    # general logging
     configure_logging(config)
     check_interval = config.get("polling_interval")
     client = TestflingerClient(config)
@@ -47,6 +169,8 @@ def start_agent():
         agent.process_jobs()
         logger.info("Sleeping for {}".format(check_interval))
         time.sleep(check_interval)
+
+    reqlog_thread.join()
 
 
 def load_config(configfile):
